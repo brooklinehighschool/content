bac7b0076330de14ec6efcdee8b8ac8e6bbdf0c2
Evan Boehs <51836263+boehs@users.noreply.github.com>
2021-11-11 16:34:26 -0500

    ts improvements

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { parse } from "https://deno.land/x/xml/mod.ts";
-import { DOMParser, Element } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 import tds from 'https://cdn.skypack.dev/turndown@7.1.1';
-import { ensureDir, ensureFile } from "https://deno.land/std@0.114.0/fs/mod.ts";
+import { ensureFile } from "https://deno.land/std@0.114.0/fs/mod.ts";
 
 // this is a horrible idea
 // because javascript is single threaded
@@ -11,10 +11,10 @@ import { ensureDir, ensureFile } from "https://deno.land/std@0.114.0/fs/mod.ts";
 // don't ever do this
 function sleep(miliseconds) {
     const currentTime = new Date().getTime();
- 
+
     while (currentTime + miliseconds >= new Date().getTime()) {
     }
- }
+}
 
 const tdsserver = new tds({})
 
@@ -24,64 +24,63 @@ const xml = await url.text()
 
 const prefix = "http://bhs.brookline.k12.ma.us/"
 
-let xmldoc = parse(xml)
+const xmldoc = parse(xml)
 
-const urls = xmldoc.urlset.url
+const urls = xmldoc?.urlset?.url
 
-async function run(url,rerun=true) {
+async function run(url: { loc: string }, rerun = true) {
     url.loc = url.loc.replace(prefix, "")
-        
+
     const path = url.loc
 
     let fetchit;
     try {
         fetchit = await fetch(prefix + url.loc)
-    } catch (e) {
+    } catch (_e) {
         console.log('taking a breather of 10 seconds, the program will be unresponsive')
         sleep(10000)
-        run(url, rerun=false)
+        run(url, rerun = false)
     }
 
     const datext = await fetchit?.text()
 
     const parsed = new DOMParser().parseFromString(datext, "text/html")
-    
-    let doc;
-    try { 
-        doc = parsed?.getElementById('content')
-    } catch (e) {
-        doc = parsed?.getElementById('wsite-content')
-    }
 
     if (parsed?.getElementById('login') != null) {
         console.log("skipping login page")
         return
     }
 
+    let doc;
+    try {
+        doc = parsed?.getElementById('content')
+    } catch (_e) {
+        doc = parsed?.getElementById('wsite-content')
+    }
+
     if (doc == null && rerun) {
         sleep(500)
         console.log('taking a breather of 0.5 seconds, the program will be unresponsive')
-        run(url, rerun=false)
+        run(url, rerun = false)
     }
-    else if (doc == null ) {
+    else if (doc == null) {
         console.log(`skipping ${url.loc}`)
         return
     }
 
-    await ensureFile(`./posts/${path}.md`)
+    await ensureFile(`./posts/${path.replace('.html', '')}.md`)
     try {
-        await Deno.writeTextFile(`./posts/${path}.md`,tdsserver.turndown(doc))
-    } catch(e) {
+        await Deno.writeTextFile(`./posts/${path}.md`, tdsserver.turndown(doc))
+    } catch (e) {
         if (rerun) {
             sleep(500)
             console.log('taking a breather of 0.5 seconds, the program will be unresponsive')
-            run(url, rerun=false)
+            run(url, rerun = false)
         }
         else {
             console.log(`skipping ${url.loc}`)
             console.log(e)
-            console.debug(doc)
-            Deno.remove(`./posts/${path}.md`)
+            Deno.remove(`./posts/${path.replace('.html', '')}.md`)
             return
         }
     }
